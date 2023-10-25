d6bb2a4d0a83222c11009770f71b4c4485a4fdd1
Ueber-James <ueberjames@gmail.com>
2023-10-25 18:39:55 -0300

    criando pastas

--- piHotelEpousadas/src/tests/exam/components/test.spec.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "../../test-utils"
-import Login from '../../../Routes/Login';
-
-test('should show login form', () => {
-  render(<Login />)
-  expect(screen.getByText('Login')).toBeInTheDocument();
-});
