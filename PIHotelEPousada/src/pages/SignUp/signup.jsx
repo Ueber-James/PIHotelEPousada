b81cc21cdc06938ee149b4da45229420d83585ac
DevLuizSouza <inboxluizsouza@gmail.com>
2023-10-26 19:23:46 -0300

    construindo o forms

---
+++ PIHotelEPousada/src/pages/SignUp/signup.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ErrorBoundary from "../../components/Error/ErrorBoundary";
+import SignUp from "../../components/SignUp";
+
+const SignUpPage = () => {
+  return (
+    <>
+      <h1>Cadastre-se</h1>
+      <ErrorBoundary>
+        <SignUp />
+      </ErrorBoundary>
+      {/* Aqui você pode adicionar o formulário de cadastro */}
+    </>
+  );
+};
+
+export default SignUpPage;
