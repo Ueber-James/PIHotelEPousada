b2ecf294bef3d3781e4d9b6425029b9111447a6a
Ueber-James <ueberjames@gmail.com>
2023-10-26 10:44:09 -0300

    mudança da logo

--- PIHotelEPousada/src/components/navbar/Navbar.jsx
+++ PIHotelEPousada/src/components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import "../navbar/navbar.css"
 import Logo from "../../assets/logo1.png";
 
+
 function NavBar() {
   return (
     <div className="navbar">
     <div className="navContainer">
-      <span className="logo"><img src={Logo} alt="" /></span>
+      <span className="logo"><a href="../../pages/home/Home"><img src={Logo} alt="" /></a></span>
       <div className="navItems">
         <button className="navButton">Register</button>
         <button className="navButton">Login</button>
