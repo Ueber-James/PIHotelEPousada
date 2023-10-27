a537ee0a7bea346355f9d217e6e6a342a403c3b6
Ueber-James <ueberjames@gmail.com>
2023-10-27 08:23:51 -0300

    criação do footer

---
+++ PIHotelEPousada/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      
+      <div className="fText">Copyright © 2022 Lamabooking.</div>
+    </div>
+  );
+};
+
+export default Footer;
