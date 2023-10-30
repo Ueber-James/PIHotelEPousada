61132431dae37697bff81a1695412bc39adca4d1
arturmnc <artur.mnc@gmail.com>
2023-10-30 09:38:15 -0300

    cards

--- PIHotelEPousada/src/pages/home/Home.jsx
+++ PIHotelEPousada/src/pages/home/Home.jsx
@@ -1,15 +1,20 @@
 import NavBar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
-
+import Card from "../../components/card/Card";
 
 
 function Home() {
+
+
     return (
         <div>
             <NavBar />
             <Header />
+            <div>
+                <Card/>
             </div>
+        </div>
     );
-};
+}
 
 export default Home;
