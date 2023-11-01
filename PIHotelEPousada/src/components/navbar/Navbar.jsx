7f20f32d7ca78880398e850401f29a8587f2a662
Ueber-James <ueberjames@gmail.com>
2023-11-01 19:31:42 -0300

    teste

--- PIHotelEPousada/src/components/navbar/Navbar.jsx
+++ PIHotelEPousada/src/components/navbar/Navbar.jsx
@@ -1,47 +1,22 @@
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
-import Logo from "../../assets/logo1.png";
-import "../navbar/navbar.css";
-=======
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "../navbar/navbar.css"
 import Logo from "../../assets/logo2.jpg";
 
->>>>>>> 7583c36a09b34c897a2d82f58c32c5dc7196d5c0
-
 function NavBar() {
   return (
     <div className="navbar">
-<<<<<<< HEAD
-      <div className="navContainer">
-        <span className="logo">
-          <a href="../../pages/home/Home">
-            <img src={Logo} alt="" />
-          </a>
-        </span>
-        <div className="navItems">
-          <Link to="/signup" className="navButton">
-            Register
-          </Link>
-          <button className="navButton">Login</button>
-        </div>
-=======
     <div className="navContainer">
       <Link to="/" className="logo"><a Link to="/"><img src={Logo} alt="" /></a></Link>
       <div className="navItems">
       <Link to="/SingUp" className="navButton">Register</Link>
         <Link to="/login" className="navButton">Login</Link>
->>>>>>> 7583c36a09b34c897a2d82f58c32c5dc7196d5c0
+
       </div>
+    </div>
     </div>
   );
 }
 
 export default NavBar;
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 7583c36a09b34c897a2d82f58c32c5dc7196d5c0
--- PIHotelEPousada/src/pages/SignUp/signup.jsx
+++ PIHotelEPousada/src/pages/SignUp/signup.jsx
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-// Importe os componentes necessários
-import Footer from "../../components/footer/Footer";
-import NavBar from "../../components/navbar/Navbar";
-import Header from "../home/Home";
-=======
+
 import React from "react";
 import ErrorBoundary from "../../components/Error/ErrorBoundary";
 import SignUp from "../SignUp/index";
->>>>>>> 7583c36a09b34c897a2d82f58c32c5dc7196d5c0
 
-import SignUp from "../signUp/Index";
 // Defina a estrutura da página
 const SignUpPage = () => {
   return (
