0660bff61e94b00f842f05209a32bc1a026c24e6
Ueber-James <ueberjames@gmail.com>
2023-11-01 18:58:53 -0300

    test

---
+++ PIHotelEPousada/babel.config.js
@@ -0,0 +1,15 @@
+// {
+//     "presets": [
+//       "@babel/preset-env",
+//       "@babel/preset-react"
+//     ],
+//     "plugins": []
+//   }
+//   module.exports = {
+//     presets: [
+//       "@babel/preset-env",
+//       "@babel/preset-react"
+//     ],
+//     plugins: []
+//   };
+  
