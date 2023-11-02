f9dfa1e165d4adfd476f8589d11bd319cd190243
Ueber-James <ueberjames@gmail.com>
2023-11-02 20:20:17 -0300

    des

--- PIHotelEPousada/src/App.jsx
+++ PIHotelEPousada/src/App.jsx
@@ -6,7 +6,7 @@ import {
   } from "react-router-dom";
   import Home from "./pages/home/Home";
   import Login from "./pages/login/Login";
-  import SingUp from "./pages/SignUp/signup";
+  
 
   
   
@@ -18,7 +18,7 @@ import {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/login" element={<Login />} />
-          <Route path="/singup" element={<SingUp />} />
+          
          
         </Routes>
       </BrowserRouter>
