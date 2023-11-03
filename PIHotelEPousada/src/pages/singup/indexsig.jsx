c9f5e9ab2366da9728456392b29aa58d8edca0f7
Ueber-James <ueberjames@gmail.com>
2023-11-03 14:19:50 -0300

    sinup

---
+++ PIHotelEPousada/src/pages/singup/indexsig.jsx
@@ -0,0 +1,80 @@
+import { Link } from "react-router-dom";
+import "./sing.css";
+// eslint-disable-next-line no-unused-vars
+
+const SignUp = () => {
+  return (
+    <div className="signup-container">
+      <div className="forms-container-signup">
+        <h1>Criar conta</h1>
+        <form>
+          <label htmlFor="name">Nome</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Digite seu nome"
+            required
+            onChange=""
+          />
+          <label htmlFor="lastname">Sobrenome</label>
+          <input
+            type="text"
+            id="lastname"
+            name="lastname"
+            placeholder="Digite seu nome"
+            required
+            onChange=""
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Digite seu email"
+            required
+            onChange=""
+          />
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Digite sua senha"
+            required
+            onChange=""
+          />
+          <div className="confirm-password-container">
+            <label htmlFor="confirmPassword">Confirme sua senha</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Digite sua senha novamente"
+              required
+              onChange=""
+            />
+          </div>
+
+          <button
+            id="buttonid"
+            type="submit"
+            className="custom-link formsButton"
+          >
+            {" "}
+            Criar conta{" "}
+          </button>
+        </form>
+
+        <h5 className="formsResgiter">
+          Já tem uma conta?{" "}
+          <Link to="/login" className="fomrsLink">
+            <strong>Iniciar sessão</strong>
+          </Link>
+        </h5>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
